2aad6a811c012fad6362071923cc5fd1f349d5ea
Amerousful <Amerousful@gmail.com>
2016-10-04 17:13:08 +0300

    add logging

--- src/main/java/Action.java
+++ src/main/java/Action.java
@@ -1,9 +1,12 @@
+import org.apache.log4j.Logger;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Action {
+    private static final Logger log = Logger.getLogger(Action.class);
+
     static public void startDriver() {
         System.setProperty("webdriver.chrome.driver", "D://Selenium//chromedriver.exe");
     }
@@ -43,6 +46,7 @@ public class Action {
             driver.findElement(locator);
             return true;
         } catch (NoSuchElementException e) {
+            log.error("The element " + locator + " was not found", e);
             return false;
         }
     }
@@ -52,7 +56,9 @@ public class Action {
             wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
             return true;
         } catch (TimeoutException ex) {
+            log.error("Time of occurrence of the element: " + locator + " is over", ex);
             return false;
+
         }
     }
     static public void exitDriver() {
