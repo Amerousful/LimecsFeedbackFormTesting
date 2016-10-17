56d8a70ac7e17d53c14bd32ec30f5132ab774957
Amerousful <Amerousful@gmail.com>
2016-10-17 12:52:08 +0300

    fixed mistakes

--- src/main/java/Data.java
+++ src/main/java/Data.java
@@ -1,37 +1,11 @@
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
-
 import java.util.Arrays;
-@RunWith(Parameterized.class)
+
 public class Data {
 
-    @Parameterized.Parameters
-    public static Iterable<Object[]> data() {
+    public static Iterable<Object[]> getTestData() {
         return Arrays.asList(new Object[][]{
                 {"myname", "test@gmail.com", "message"},
                 {"myname", "test@", "message"}
         });
     }
-
-    private static String name;
-    private static String email;
-    private static String message;
-
-    public Data (String name, String email, String message) {
-        this.name = name;
-        this.email = email;
-        this.message = message;
-    }
-
-    static String getName() {
-        return name;
-    }
-    static String getEmail() {
-        return email;
-    }
-    static String getMessage() {
-        return message;
-    }
-
 }
