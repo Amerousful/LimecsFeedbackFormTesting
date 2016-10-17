38d29532bdcd23c735efa3da74df2dc8960a55ee
Amerousful <Amerousful@gmail.com>
2016-10-17 12:46:53 +0300

    small refactoring

--- src/main/java/Action.java
+++ src/main/java/Action.java
@@ -13,42 +13,27 @@ public class Action {
     static WebDriver driver = new ChromeDriver();
     static WebDriverWait wait = new WebDriverWait(driver, 10);
 
-    static public void clickButton(By fieldlocator) {
-        elementWait(fieldlocator);
-        WebElement element = driver.findElement(fieldlocator);
-        element.click();
+    static public void goToSite() {
+        driver.get(Locators.getUrl());
     }
-
+    static public void goToContact() {
+        clickButton(Locators.getContactButtonPath());
+    }
     static public void fillField(String fielddata, By fieldlocator) {
         elementWait(fieldlocator);
         WebElement element = driver.findElement(fieldlocator);
         element.sendKeys(fielddata);
     }
-
-    static public void goToSite() {
-        driver.get(Locators.getUrl());
-    }
-
-    static public void goToContact() {
-        clickButton(Locators.getContactButtonPath());
-    }
     static public void clickSendButton() {
         clickButton(Locators.getSendButtonPath());
     }
-
-    static public boolean confirmMessage() {
-        return elementPresent(Locators.getConfirmMessage());
+    static public void clickButton(By fieldlocator) {
+        elementWait(fieldlocator);
+        WebElement element = driver.findElement(fieldlocator);
+        element.click();
     }
-
-
-    static protected boolean elementPresent(By locator) {
-        try {
-            driver.findElement(locator);
-            return true;
-        } catch (NoSuchElementException e) {
-            log.error("The element " + locator + " was not found", e);
-            return false;
-        }
+    static public boolean confirmMessage() {
+        return elementWait(Locators.getConfirmMessage());
     }
 
     static protected boolean elementWait(By locator) {
@@ -56,9 +41,8 @@ public class Action {
             wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
             return true;
         } catch (TimeoutException ex) {
-            log.error("Time of occurrence of the element: " + locator + " is over", ex);
+            log.error("Time of occurrence of the element: " + locator + " is over");
             return false;
-
         }
     }
     static public void exitDriver() {
